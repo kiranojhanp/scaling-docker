97f9d5532e08e64f019aefd54584097a1cd01c38
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-06-21 19:25:17 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# How to scale a docker container?
+
+`docker compose up --scale api=10`
---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const os = require("os")
+
+const app = express()
+
+app.use("/", async (req, res) => {
+	console.log(`I am sending a response ${os.hostname()}`)
+	res.json({ message: "Ok it works...", hostname: os.hostname() })
+})
+
+app.listen(3000, () => console.log("@ 🚀 localhost:3000"))
